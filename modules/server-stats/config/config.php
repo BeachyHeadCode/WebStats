a2bfcdaea7038fa5773e3cfa46bc39a9b6f98001
Nick Smith <admin@nicholas-smith.tk>
2015-06-07 10:37:33 -0400

    Allowed server stats to show in menu

--- modules/server-stats/config/config.php
+++ modules/server-stats/config/config.php
@@ -1,5 +1,8 @@
 <?php
 	include_once('modules/'.WS_CONFIG_STATS_PLUGIN.'/config/config.php');
 	$menuname='Server statistics';
-	$plugintype["SERVER-STATS"]=true;
+	if ($plugintype["Stats"]===true) {
+		$on=true;
+		$plugintype["SERVER-STATS"]=true;
+	}
 ?>
